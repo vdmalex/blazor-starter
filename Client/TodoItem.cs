0b9bf02f21de6231e5787e99d0bc7b8cfd15aa43
Alexander van der Meulen <amvandermeulen0234@eagle.fgcu.edu>
2021-09-27 19:00:07 -0400

    added todo, changed icon, edited IPO pageadded ipo page and changed index

---
+++ Client/TodoItem.cs
@@ -0,0 +1,7 @@
+public class TodoItem
+{
+    public string Title { get; set; }
+    // get and set methods for the Title property
+    public bool IsDone { get; set; }
+    // get and set methods for the IsDone property
+}
